373d290d27a08c277a90e0382718f96fa6e1388c
TheParasitoidChico <christianconnors@nevada.unr.edu>
2022-02-10 15:14:33 -0800

    automated push, but also... the python repository is finally here. wawza.

---
+++ PYTHON_BIOINFORMATICS_dust/Python1.py
@@ -0,0 +1,8 @@
+# Just some python review, (for your viewing pleasure)
+print("FINALLY, SOME PYTHON")
+print("What does a sexy snake wear?")
+what = str(input("Go ahead, ask me." + '\n'))
+if what == 'I dont care':
+    print("oh, okay")
+else:
+    print("A PYTHONG!")
